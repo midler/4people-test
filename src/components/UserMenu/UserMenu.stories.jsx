bea2c0381faefd78dbb234c048005001a5eade9a
Andrey Seredkin <seredkin@htmlacademy.ru>
2022-12-12 19:53:45 +0400

    Add user-menu

---
+++ src/components/UserMenu/UserMenu.stories.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import "../../styles/index.scss";
+
+import UserMenu from "./UserMenu";
+import UserMenuItem from "../UserMenuLink/UserMenuLink";
+
+export default {
+  title: "UserMenu",
+  component: UserMenu,
+};
+
+// const Template = (args) => (
+//   <UserMenu {...args}>
+//     <UserMenuItem>sss</UserMenuItem>
+//     <UserMenuItem>sss</UserMenuItem>
+//     <UserMenuItem>sss</UserMenuItem>
+//     <UserMenuItem>sss</UserMenuItem>
+//   </UserMenu>
+// );
+
+// export const UserMenuAsLink = Template.bind({});
+// UserMenuAsLink.args = {};
