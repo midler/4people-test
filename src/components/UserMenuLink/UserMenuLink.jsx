806ae3c6159fbcab6558409bc8a951ca561fefb0
Andrey Seredkin <seredkin@htmlacademy.ru>
2022-12-14 15:10:43 +0400

    Change USerMenuLink

--- src/components/UserMenuLink/UserMenuLink.jsx
+++ src/components/UserMenuLink/UserMenuLink.jsx
@@ -1,16 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
+import clsx from "clsx";
 
 import "./UserMenuLink.scss";
 
 export const UserMenuLink = ({ ...props }) => {
   return props.href ? (
-    <a className={`user-menu-link ${props.className}`} href={props.href}>
-      {props.children}
+    <a className={clsx("user-menu-link", props.className)} href={props.href}>
+      {props.icon ? (
+        <span className="user-menu-link__icon">{props.icon}</span>
+      ) : (
+        ""
+      )}
+
+      <span className="user-menu-link__content">{props.children}</span>
     </a>
   ) : (
-    <button type="button" className={`user-menu-link ${props.className}`}>
-      {props.children}
+    <button type="button" className={clsx("user-menu-link", props.className)}>
+      {props.icon ? (
+        <span className="user-menu-link__icon">{props.icon}</span>
+      ) : (
+        ""
+      )}
+
+      <span className="user-menu-link__content">{props.children}</span>
     </button>
   );
 };
@@ -18,9 +31,11 @@ export const UserMenuLink = ({ ...props }) => {
 UserMenuLink.propTypes = {
   children: PropTypes.string || PropTypes.element,
   href: PropTypes.string,
+  icon: PropTypes.element,
 };
 
 UserMenuLink.defaultProps = {
   href: "",
   className: "",
+  icon: "",
 };
