0212252dc3792bdab30a87d08c1d0fc33494d613
Andrey Seredkin <seredkin@htmlacademy.ru>
2022-12-14 15:23:12 +0400

    Add markup and use styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useRef } from "react";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper";
 
 import { Link } from "./components/Link/Link.jsx";
 import { Logo } from "./components/Logo/Logo.jsx";
 import { SearchInput } from "./components/SearchInput/SearchInput.jsx";
 import { UserMenuLink } from "./components/UserMenuLink/UserMenuLink.jsx";
 import List from "./components/List/List.jsx";
+import Slide from "./components/Slide/Slide.jsx";
+import { Button } from "./components/Button/Button.jsx";
 
 import "./App.scss";
+
+import "swiper/css";
+import "swiper/css/pagination";
+
+import { ReactComponent as UserNavIcon1 } from "./assets/svg/item1.svg";
+import { ReactComponent as UserNavIcon2 } from "./assets/svg/item2.svg";
+import { ReactComponent as UserNavIcon3 } from "./assets/svg/item3.svg";
+import { ReactComponent as UserNavIcon4 } from "./assets/svg/item4.svg";
+
 import cat1 from "./assets/images/categories-1.jpg";
 import cat2 from "./assets/images/categories-2.jpg";
 import cat3 from "./assets/images/categories-3.jpg";
@@ -17,6 +31,23 @@ import cat6 from "./assets/images/categories-6.jpg";
 import paymentMethodImage from "./assets/images/payments-method.png";
 
 function App() {
+  const pagination = {
+    el: ".slider-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return (
+        '<button class="slider-pagination__item ' +
+        className +
+        '"><span class="visually-hidden">Slide ' +
+        (index + 1) +
+        "</span></button>"
+      );
+    },
+  };
+
+  const navigationPrevRef = useRef(null);
+  const navigationNextRef = useRef(null);
+
   return (
     <div className="main-page">
       <header className="main-page__header main-header">
@@ -31,10 +62,19 @@ function App() {
           <Logo className="main-header__logo" />
           <SearchInput className="main-header__search" />
           <List className="user-menu" horizontal>
-            <UserMenuLink>Lists</UserMenuLink>
-            <UserMenuLink>Account</UserMenuLink>
-            <UserMenuLink>Quick Order</UserMenuLink>
-            <UserMenuLink>Cart</UserMenuLink>
+            <UserMenuLink icon={<UserNavIcon1 />}>Lists</UserMenuLink>
+            <UserMenuLink icon={<UserNavIcon2 />}>Account</UserMenuLink>
+            <UserMenuLink icon={<UserNavIcon3 />}>Quick Order</UserMenuLink>
+            <UserMenuLink
+              icon={
+                <span className="cart-button">
+                  <span className="cart-button__count">32</span>
+                  <UserNavIcon4 />
+                </span>
+              }
+            >
+              Cart
+            </UserMenuLink>
           </List>
         </div>
         <div className="main-header__layer3">
@@ -60,120 +100,216 @@ function App() {
               </Link>
             </List>
           </nav>
-          <Link href="tel:+844434672">+(844) 434-672</Link>
+          <Link href="tel:+844434672" className="main-phone">
+            +(844) 434-672
+          </Link>
         </div>
       </header>
       <main className="main-page__main-section main-section">
-        <section className="slider">
+        <section className="slider-section main-section__slider">
           <h2 className="visually-hidden">Slider</h2>
-          <ul className="slider__list">
-            <li className="slider__item">
-              <section className="slide">
-                <h3 className="slide__title">
-                  Fiberglass and specialty metals are in stock across the United
-                  States
-                </h3>
-                <button type="button">Shop Now</button>
-              </section>
-            </li>
-            <li className="slider__item">
-              <section className="slide">
-                <h3 className="slide__title">
-                  Fiberglass and specialty metals are in stock across the United
-                  States
-                </h3>
-                <button type="button">Shop Now</button>
-              </section>
-            </li>
-            <li className="slider__item">
-              <section className="slide">
-                <h3 className="slide__title">
-                  Fiberglass and specialty metals are in stock across the United
-                  States
-                </h3>
-                <button type="button">Shop Now</button>
-              </section>
-            </li>
-            <li className="slider__item">
-              <section className="slide">
-                <h3 className="slide__title">
-                  Fiberglass and specialty metals are in stock across the United
-                  States
-                </h3>
-                <button type="button">Shop Now</button>
-              </section>
-            </li>
-          </ul>
-          <List className="slider__pagination">
-            <li>
-              <span className="visually-hidden">Slide 1</span>
-            </li>
-            <li>
-              <span className="visually-hidden">Slide 2</span>
-            </li>
-            <li>
-              <span className="visually-hidden">Slide 3</span>
-            </li>
-            <li>
-              <span className="visually-hidden">Slide 4</span>
-            </li>
-          </List>
+          <Swiper
+            pagination={pagination}
+            modules={[Pagination]}
+            className="slider"
+          >
+            <SwiperSlide className="slider__item">
+              <Slide></Slide>
+            </SwiperSlide>
+            <SwiperSlide className="slider__item">
+              <Slide></Slide>
+            </SwiperSlide>
+            <SwiperSlide className="slider__item">
+              <Slide></Slide>
+            </SwiperSlide>
+            <SwiperSlide className="slider__item">
+              <Slide></Slide>
+            </SwiperSlide>
+            <div className="slider-pagination"></div>
+          </Swiper>
         </section>
 
-        <section className="categories">
-          <h2 className="categories__title">Categories</h2>
-          <List className="categories__switch" horizontal>
-            <button type="button">
-              <span className="visually-hidden">previus categories</span>
-            </button>
-            <button type="button">
-              <span className="visually-hidden">next categories</span>
-            </button>
-          </List>
-          <List className="categories-list">
-            <>
-              <img src={cat1} alt="" width="197" height="197" />
-              <a href="/">Abrasives</a>
-            </>
-            <>
-              <img src={cat2} alt="" width="197" height="197" />
-              <a href="/">Electrical</a>
-            </>
-            <>
-              <img src={cat3} alt="" width="197" height="197" />
-              <a href="/">Welding</a>
-            </>
-            <>
-              <img src={cat4} alt="" width="197" height="197" />
-              <a href="/">Safety</a>
-            </>
-            <>
-              <img src={cat5} alt="" width="197" height="197" />
-              <a href="/">Fasteners</a>
-            </>
-            <>
-              <img src={cat6} alt="" width="197" height="197" />
-              <a href="/">Hand Tools</a>
-            </>
-          </List>
+        <section className="categories main-section__categories">
+          <div className="categories__title-block">
+            <h2 className="categories__title">Categories</h2>
+            <div className="categories__switch">
+              <button
+                className="categories__switch-button categories__switch-button--prev"
+                ref={navigationPrevRef}
+              >
+                <div className="visually-hidden">Previous category</div>
+              </button>
+              <button
+                className="categories__switch-button categories__switch-button--next"
+                ref={navigationNextRef}
+              >
+                <div className="visually-hidden">Next category</div>
+              </button>
+            </div>
+          </div>
+          <Swiper
+            modules={[Pagination, Navigation]}
+            className="categories-list"
+            slidesPerView={6}
+            spaceBetween={42}
+            navigation={{
+              prevEl: navigationPrevRef.current,
+              nextEl: navigationNextRef.current,
+            }}
+          >
+            <SwiperSlide className="categories-list__item">
+              <a className="category" href="/">
+                <img
+                  className="category__image"
+                  src={cat1}
+                  alt=""
+                  width="197"
+                  height="197"
+                />
+                Abrasives
+              </a>
+            </SwiperSlide>
+            <SwiperSlide className="categories-list__item">
+              <a className="category" href="/">
+                <img
+                  className="category__image"
+                  src={cat2}
+                  alt=""
+                  width="197"
+                  height="197"
+                />
+                Electrical
+              </a>
+            </SwiperSlide>
+            <SwiperSlide className="categories-list__item">
+              <a className="category" href="/">
+                <img
+                  className="category__image"
+                  src={cat3}
+                  alt=""
+                  width="197"
+                  height="197"
+                />
+                Welding
+              </a>
+            </SwiperSlide>
+            <SwiperSlide className="categories-list__item">
+              <a className="category" href="/">
+                <img
+                  className="category__image"
+                  src={cat4}
+                  alt=""
+                  width="197"
+                  height="197"
+                />
+                Safety
+              </a>
+            </SwiperSlide>
+            <SwiperSlide className="categories-list__item">
+              <a className="category" href="/">
+                <img
+                  className="category__image"
+                  src={cat5}
+                  alt=""
+                  width="197"
+                  height="197"
+                />
+                Fasteners
+              </a>
+            </SwiperSlide>
+            <SwiperSlide className="categories-list__item">
+              <a className="category" href="/">
+                <img
+                  className="category__image"
+                  src={cat6}
+                  alt=""
+                  width="197"
+                  height="197"
+                />
+                Hand Tools
+              </a>
+            </SwiperSlide>
+            <SwiperSlide className="categories-list__item">
+              <a className="category" href="/">
+                <img
+                  className="category__image"
+                  src={cat6}
+                  alt=""
+                  width="197"
+                  height="197"
+                />
+                Hand Tools
+              </a>
+            </SwiperSlide>
+            <SwiperSlide className="categories-list__item">
+              <a className="category" href="/">
+                <img
+                  className="category__image"
+                  src={cat6}
+                  alt=""
+                  width="197"
+                  height="197"
+                />
+                Hand Tools
+              </a>
+            </SwiperSlide>
+            <SwiperSlide className="categories-list__item">
+              <a className="category" href="/">
+                <img
+                  className="category__image"
+                  src={cat6}
+                  alt=""
+                  width="197"
+                  height="197"
+                />
+                Hand Tools
+              </a>
+            </SwiperSlide>
+            <SwiperSlide className="categories-list__item">
+              <a className="category" href="/">
+                <img
+                  className="category__image"
+                  src={cat6}
+                  alt=""
+                  width="197"
+                  height="197"
+                />
+                Hand Tools
+              </a>
+            </SwiperSlide>
+            <SwiperSlide className="categories-list__item">
+              <a className="category" href="/">
+                <img
+                  className="category__image"
+                  src={cat6}
+                  alt=""
+                  width="197"
+                  height="197"
+                />
+                Hand Tools
+              </a>
+            </SwiperSlide>
+          </Swiper>
         </section>
-        <section className="promo">
-          <h2 className="promo">
-            Large Inventory Of Power Tools & Accessories
-          </h2>
-          <p>
-            Find the Aluminum and Stainless Steel items for your project in
-            stock and ready to ship today! Avoid project delays and long lead
-            times in a challenging supply chain for specialty metals and
-            non-ferrous items. We are well stocked and here to serve you with
-            guided support.
-          </p>
-          <button type="button">Shop Now</button>
+        <section className="promo main-section__promo">
+          <div className="promo__content">
+            <h2 className="promo__title">
+              Large Inventory Of Power Tools & Accessories
+            </h2>
+            <p>
+              Find the Aluminum and Stainless Steel items for your project in
+              stock and ready to ship today! Avoid project delays and long lead
+              times in a challenging supply chain for specialty metals and
+              non-ferrous items. We are well stocked and here to serve you with
+              guided support.
+            </p>
+            <Button>Shop Now</Button>
+          </div>
         </section>
 
-        <section className="advantages">
-          <h2 className="advantages">Advantages</h2>
-          <List horizontal>
+        <section className="advantages main-section__advantages">
+          <h2 className="visually-hidden">Advantages</h2>
+          <List horizontal className="advantages-list">
             <p>Products Catalog</p>
 
             <p>24/7 Customer Support</p>
@@ -184,54 +320,39 @@ function App() {
       </main>
 
       <footer className="main-footer">
-        <div>
+        <div className="main-footer__row">
           <section className="footer-links">
             <h2 className="footer-links__title">Helpful Links</h2>
-            <List>
+            <List className="footer-links-list">
               <a href="/">Buyer’s Guides</a>
-
               <a href="/">Calculators</a>
-
               <a href="/">All Brands</a>
-
               <a href="/">Catalg Updates</a>
             </List>
           </section>
           <section className="footer-links">
             <h2 className="footer-links__title">Customer Services</h2>
-            <List>
+            <List className="footer-links-list">
               <a href="/">Express Delivery</a>
-
               <a href="/">Privacy Statement</a>
-
               <a href="/">Terms of Use</a>
-
               <a href="/">Pricing Policy</a>
-
               <a href="/">Returns and Refunds</a>
-
               <a href="/">Help and FAQ</a>
-
               <a href="/">Return Points</a>
-
               <a href="/">Returns and Orders</a>
             </List>
           </section>
 
           <section className="footer-links">
             <h2 className="footer-links__title">Support</h2>
-            <List>
+            <List className="footer-links-list">
               <a href="/">Contact Us</a>
-
               <a href="/">FAQ</a>
-
               <a href="/">Feedback</a>
-
               <a href="/">Live Chat</a>
-
               <a href="/">My Account</a>
-
-              <a href="tel:+844434672">+(844) 434-672</a>
+              <a href="tel:+844434672" class="main-phone main-phone--footer">+(844) 434-672</a>
             </List>
           </section>
         </div>
@@ -261,7 +382,7 @@ function App() {
           </section>
         </div>
         <div>
-          <p>© «Brand» 2022, All Rights Reserved</p>
+          <p className="all-right-reserved">© «Brand» 2022, All Rights Reserved</p>
         </div>
       </footer>
 
